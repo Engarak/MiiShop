13d8839c948c06b6b4d4d6beb2649d431f09e032
Engarak <engarak@tutanota.com>
2019-05-19 01:25:02 -0400

    Removed un-needed tag
    I removed an unneeded window style tag on the old version.

--- start.bat
+++ start.bat
@@ -1,3 +1,4 @@
 @echo off
 pushd %~dp0
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe .\miishop.ps1
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe .\miiShop_Install.ps1
+pause
